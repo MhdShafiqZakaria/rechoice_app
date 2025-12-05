cdc509ea438f801b90b9eb24d44ea97c5dbd65e2
uulAfif <afifmorny@gmail.com>
2025-12-05 23:54:54 +0800

    gambar and text nama app

--- lib/pages/dashboard.dart
+++ lib/pages/dashboard.dart
@@ -5,6 +5,58 @@ class Dashboard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text('Dashboard')));
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          width: double.infinity,
+          color: Colors.white, 
+          child: SingleChildScrollView(
+            padding: const EdgeInsets.all(2.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const SizedBox(height: 16),
+
+                //kotak biru
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  child: Container(
+                    width: double.infinity, 
+                    height: 60, 
+                    padding: const EdgeInsets.all(14.0),
+                    decoration: BoxDecoration(
+                      color: Colors.blueAccent,
+                      borderRadius: BorderRadius.circular(16.0),
+                    ),
+                    
+                    //logo n text
+                    child: Row(
+                      children: [
+                        Image.asset(
+                          'assets/images/logo.png', 
+                          width: 35, 
+                          height: 35,
+                        ),
+                        const SizedBox(width: 12),
+                        const Text(
+                          'ReChoice: UNIMAS Preloved Item',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 10,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+
+                const SizedBox(height: 16),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
